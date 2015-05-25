ea9dc2fa115c4c9e876a0226296c24edb5699d7f
Amrita Virdi <virdi.amrita09@gmail.com>
2015-05-24 20:51:42 -0700

    added hello world files to the project

---
+++ Comp397-Lesson1-Part2/Scripts/game.ts
@@ -0,0 +1,16 @@
+﻿class person {
+    //------- Constructor
+    constructor() {
+    }
+    //public method
+    public saysHello() {
+        console.log("hello world");
+    }
+}
+function init() {
+    var myPerson = new myPerson();
+    myPerson.saysHello();
+}
+
+       
+
